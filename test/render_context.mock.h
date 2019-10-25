9cee03c3e77821087fac9dd86138497b4d901d39
Peter Kuehne <peter.kuehne@gmail.com>
2019-10-25 17:02:07 +0100

    Migrate to newer MOCK_METHOD syntax

--- test/render_context.mock.h
+++ test/render_context.mock.h
@@ -6,25 +6,34 @@ public:
     MockRenderContext(SwearJar::CursesInterface& curses)
         : RenderContext(curses, 0) {
     }
-    MOCK_METHOD0(beginRender, void());
-    MOCK_METHOD0(endRender, void());
-    MOCK_CONST_METHOD1(reverse, void(bool));
-    MOCK_CONST_METHOD1(blink, void(bool));
-    MOCK_CONST_METHOD6(clearArea, void(unsigned int, unsigned int, unsigned int,
-                                       unsigned int, short, short));
-    MOCK_CONST_METHOD5(drawText, void(unsigned int, unsigned int,
-                                      const std::string&, short, short));
-    MOCK_CONST_METHOD5(drawText, void(unsigned int, unsigned int,
-                                      const std::wstring&, short, short));
-    MOCK_CONST_METHOD3(drawChar, void(unsigned int, unsigned int, char));
-    MOCK_CONST_METHOD3(drawChar, void(unsigned int, unsigned int, wchar_t));
-    MOCK_CONST_METHOD5(drawChar,
-                       void(unsigned int, unsigned int, char, short, short));
-    MOCK_CONST_METHOD5(drawChar,
-                       void(unsigned int, unsigned int, wchar_t, short, short));
-    MOCK_CONST_METHOD6(drawBorder,
-                       void(unsigned int, unsigned int, unsigned int,
-                            unsigned int, short, short));
+    MOCK_METHOD(void, beginRender, (), (override));
+    MOCK_METHOD(void, endRender, (), (override));
+    MOCK_METHOD(void, reverse, (bool), (override, const));
+    MOCK_METHOD(void, blink, (bool), (override, const));
+    MOCK_METHOD(void, clearArea,
+                (unsigned int, unsigned int, unsigned int, unsigned int, short,
+                 short),
+                (override, const));
+    MOCK_METHOD(void, drawText,
+                (unsigned int, unsigned int, const std::string&, short, short),
+                (override, const));
+    MOCK_METHOD(void, drawText,
+                (unsigned int, unsigned int, const std::wstring&, short, short),
+                (override, const));
+    MOCK_METHOD(void, drawChar, (unsigned int, unsigned int, char),
+                (override, const));
+    MOCK_METHOD(void, drawChar, (unsigned int, unsigned int, wchar_t),
+                (override, const));
+    MOCK_METHOD(void, drawChar,
+                (unsigned int, unsigned int, char, short, short),
+                (override, const));
+    MOCK_METHOD(void, drawChar,
+                (unsigned int, unsigned int, wchar_t, short, short),
+                (override, const));
+    MOCK_METHOD(void, drawBorder,
+                (unsigned int, unsigned int, unsigned int, unsigned int, short,
+                 short),
+                (override, const));
     MOCK_METHOD(void, addOffsets, (unsigned int x, unsigned int y),
                 (const, override));
     MOCK_METHOD(void, clearOffsets, (unsigned int x, unsigned int y),
