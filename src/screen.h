1615e045dc875d4c827123e3aa12abb2ae83c2c4
Peter Kuehne <peter.kuehne@gmail.com>
2021-02-24 21:18:46 +0000

    UnhandledKeys quits by default

--- src/screen.h
+++ src/screen.h
@@ -12,26 +12,49 @@ class Window;
 class Screen {
 public:
     explicit Screen(std::shared_ptr<CursesInterface>&& curses);
+
+    /// Call this with `new CursesWrapper()`. Screen will take ownership of the
+    /// pointer.
     explicit Screen(CursesInterface* curses);
-    
+
     ~Screen();
+
+    /// Sets up the underlying drawing system. Call this when you are ready to
+    /// hand over control of the screen to SwearJar
     void initialize();
+
+    // Hand over control to SwearJar
     void run();
 
+    /// Get access to the underlying Curses wrapper
     CursesInterface& curses() {
         return *m_curses;
     }
+
+    /// Create a new window and make it topmost within the application
     template <typename T> T& createWindow();
+
+    /// Remove the top window
     void popWindow();
+
+    /// Get a list of all windows the Screen is currently managing
     const std::vector<std::unique_ptr<Window>>& windows() const {
         return m_windows;
     }
 
+    /// End control of the screen
     void quit() {
         m_quit = true;
     }
+
+    /// This function will be called for any keyboard keys that are not handled
+    /// by SwearJar directly. Will by default quit the application when any key
+    /// is pressed
     std::function<void(const KeyEvent&)> unhandledKeys =
-        [](const KeyEvent& /* e */) {};
+        [this](const KeyEvent& /* e */) { quit(); };
+
+    /// This function will be called whenever the screen is resized. SwearJar
+    /// will re-align widgets automatically
     std::function<void()> screenResized = []() {};
 
 private:
